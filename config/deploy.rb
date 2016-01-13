d03f845caae0c4dd6c8f2de776a2f98aec12a443
tjinjin <tjinjinprogram@gmail.com>
2016-01-14 00:15:54 +0900

    とりあえず

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,5 +4,5 @@ set :deploy_to, '/var/www'
 set :deploy_roles, 'www'
 set :stretcher_hooks, 'config/stretcher.yml.erb'
 set :local_tarball_name, 'rails-applicaiton.tar.gz'
-set :stretcher_src, "s3://#{ENV['S3_BUCKET']}/assets/rails-application-#{env.now}.tgz"
-set :manifest_path, "s3://#{ENV['S3_BUCKET']}/manifests/"
+set :stretcher_src, "s3://tjinjin-try-stretcher/assets/rails-application-#{env.now}.tgz"
+set :manifest_path, "s3://tjinjin-tyr-stretcher/manifests/"

cd9ffd7f0aa4f55f59025b8ef07771af0e2fa966
tjinjin <tjinjinprogram@gmail.com>
2016-01-14 00:27:59 +0900

    Fix typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,4 +5,4 @@ set :deploy_roles, 'www'
 set :stretcher_hooks, 'config/stretcher.yml.erb'
 set :local_tarball_name, 'rails-applicaiton.tar.gz'
 set :stretcher_src, "s3://tjinjin-try-stretcher/assets/rails-application-#{env.now}.tgz"
-set :manifest_path, "s3://tjinjin-tyr-stretcher/manifests/"
+set :manifest_path, "s3://tjinjin-try-stretcher/manifests/"

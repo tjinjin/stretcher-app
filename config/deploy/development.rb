adcef2c41c6ea1130fff6e9c96ce1670b489156a
tjinjin <tjinjinprogram@gmail.com>
2016-01-13 23:56:07 +0900

    一旦sudoできるユーザに

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,7 +3,7 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :url, 'http://localhost/'
 set :repo_url, 'https://github.com/tjinjin/stretcher-app'
 set :ssh_options, {
-   user: 'app',
+   user: 'vagrant',
    keys: %w(~/.vagrant.d/insecure_private_key),
    forward_agent: false,
    auth_methods: %w(publickey)

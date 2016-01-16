8f4acbc21f524f015090e428ee24aa6abd210013
tjinjin <tjinjinprogram@gmail.com>
2016-01-17 01:19:59 +0900

    Fix deploy

---
+++ config/unicorn/development.rb
@@ -0,0 +1,33 @@
+rails_root = '/var/www/my_app_name'
+
+ENV['BUNDLE_GEMFILE'] = rails_root + "current/Gemfile"
+
+worker_processes 2
+working_directory "#{rails_root}/current"
+
+timeout 30
+
+stderr_path 'log/unicorn_stderr.log'
+stdout_path 'log/unicorn_stderr.log'
+
+listen 8080
+pid "#{rails_root}/current/tmp/pids/unicorn.pid"
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end

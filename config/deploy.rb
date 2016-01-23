e1480ed61536cb8d9e663d8ad26b54379528809f
tjinjin <tjinjinprogram@gmail.com>
2016-01-23 17:25:29 +0900

    Change s3 path

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,5 +4,5 @@ set :deploy_to, '/var/www/stretcher_app'
 set :deploy_roles, 'web'
 set :stretcher_hooks, 'config/stretcher.yml.erb'
 set :local_tarball_name, 'rails-applicaiton.tar.gz'
-set :stretcher_src, "s3://tjinjin-try-stretcher/assets/rails-application-#{env.now}.tgz"
-set :manifest_path, "s3://tjinjin-try-stretcher/manifests"
+set :stretcher_src, "s3://tjinjin-backend-stretcher/assets/rails-application-#{env.now}.tgz"
+set :manifest_path, "s3://tjinjin-backend-stretcher/manifests"

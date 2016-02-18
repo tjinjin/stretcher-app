a683f8ac4f3c2a4906922216132f865239bf9511
tjinjin <tjinjinprogram@gmail.com>
2016-02-19 00:14:51 +0900

    Fix

--- script/register_kv.sh
+++ script/register_kv.sh
@@ -7,4 +7,4 @@ node_name=`curl -s localhost:8500/v1/agent/self | python -c "exec(\"import json,
 log=`cat $2`
 
 
-curl -X PUT -d "hello world" "localhost:8500/v1/kv/dashboard/application/${node_name}?flags=${datetime_code}"
+curl -X PUT -d "$log" "localhost:8500/v1/kv/dashboard/application/${node_name}?flags=${datetime_code}"
